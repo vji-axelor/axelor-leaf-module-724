debb083fe99df0a18a2854babdff1312ad7b11a7
vikas jivani <vikasjivani16@gmail.com>
2025-07-29 17:46:09 +0200

    Change report formate

--- axelor-purchase/src/main/java/com/axelor/apps/purchase/web/PurchaseOrderController.java
+++ axelor-purchase/src/main/java/com/axelor/apps/purchase/web/PurchaseOrderController.java
@@ -18,6 +18,21 @@
  */
 package com.axelor.apps.purchase.web;
 
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.annotation.Nullable;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.axelor.app.AppSettings;
 import com.axelor.apps.ReportFactory;
 import com.axelor.apps.account.db.FiscalPosition;
 import com.axelor.apps.account.db.PaymentMode;
@@ -56,20 +71,13 @@ import com.axelor.meta.schema.actions.ActionView.ActionViewBuilder;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
+import com.axelor.rpc.Request;
 import com.axelor.utils.db.Wizard;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.inject.Singleton;
-import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import javax.annotation.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Singleton
 public class PurchaseOrderController {
@@ -574,6 +582,7 @@ public class PurchaseOrderController {
   public void printPurchaseOrderQuote(ActionRequest request, ActionResponse response)
       throws AxelorException {
     PurchaseOrder purchaseOrder = request.getContext().asType(PurchaseOrder.class);
+    
     String fileLink =
         ReportFactory.createReport("PurchaseOrderQuote.rptdesign", "Quotation")
             .addParam("PurchaseOrderId", purchaseOrder.getId())
@@ -582,4 +591,5 @@ public class PurchaseOrderController {
 
     response.setView(ActionView.define("Quotation").add("html", fileLink).map());
   }
+  
 }
